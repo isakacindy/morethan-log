1bfe3d8fa265a5bd540a098ec561b57ca3ea870b
isakacindy <94227598+isakacindy@users.noreply.github.com>
2026-01-08 16:31:22 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "meanvelop",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Data Analyst",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    email: "isakacindy@gmail.com",
     linkedin: "morethanmin",
     github: "morethanmin",
     instagram: "",
